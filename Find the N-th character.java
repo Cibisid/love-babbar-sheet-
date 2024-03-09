586e345b5a7d8d7ed5807a0ac0e46778a5af0d09
Cibisid <157216208+Cibisid@users.noreply.github.com>
2024-03-09 23:00:36 +0530

    Add files via upload

---
+++ Find the N-th character.java
@@ -0,0 +1,22 @@
+class Solution
+{
+    public char nthCharacter(String s, int r, int n)
+    {
+        //code here
+        if(r==0) return s.charAt(n);
+        while(r>0){
+            String temp="";
+            for(int i=0;i<=n;i++){
+                if(s.charAt(i)=='0'){
+                    temp=temp+"01";
+                }else{
+                    temp=temp+"10";
+                }
+            }
+            s=temp;
+            r--;
+        }
+        return s.charAt(n);
+        
+    }
+}
